8bfca23fb72b9b574fa200e5cdd65371a4ccd8cd
Daniel <daniel.r.heiniger@gmail.com>
2024-09-05 22:53:05 -0500

    Add option for throttling

--- src/scour/core.clj
+++ src/scour/core.clj
@@ -39,7 +39,6 @@
 (defn- get-domain [url]
   (:host (bean (as-url url))))
 
-;;TODO multi-thread
 (defn- next-hrefs
   ([domain ^Document doc saved-pages]
    (next-hrefs domain doc saved-pages {}))
@@ -55,6 +54,14 @@
     (cond limit-reached? (do (log/info "Limit of" limit "reached.") true)
           (not (seq pages)) (do (log/info "All pages processed.") true)
           :else false)))
+
+(defn throttle [throttle-amt]
+  (let [amt (if (sequential? throttle-amt)
+              (let [[min max] throttle-amt] (+ (rand-int max) min))
+              (or throttle-amt 0))]
+    (when (pos? amt)
+      (log/info "Waiting " amt "ms..")
+      (Thread/sleep amt))))
 
 (defn scan
   "Returns a collection of pages on the same domain as url.
@@ -72,9 +79,14 @@
 
   The number of pages to scan before returning.  This is only an approximate limit
   because of the way pages are scanned in batches based on the number of
-  hrefs found on a prior page"
+  hrefs found on a prior page
+
+  :throttle-amt
+
+  The amount of time to wait between each page request.  Can be a number in ms
+  or a range of numbers in ms to add randomness such as [100 50000]"
   ([url] (scan url {}))
-  ([url {:keys [timeout limit] :or {timeout 20000} :as opts}]
+  ([url {:keys [timeout limit throttle-amt] :or {timeout 20000} :as opts}]
    (let [first-page [(normalize-url url) (download-page url)]]
      (loop [[page & rst :as pages] (set [first-page])
             saved-pages #{first-page}]
@@ -83,11 +95,12 @@
                saved-urls (map first saved-pages)
                next-hrefs (next-hrefs (get-domain url) @content saved-urls opts)
                next-pages (map (fn [href] [href (download-page href timeout)]) next-hrefs)]
+           (throttle throttle-amt)
            (recur (concat rst next-pages) (concat saved-pages next-pages)))
          saved-pages)))))
 
 (comment
-  (time (def results (scan "https://google.com" {:limit 100})));"Elapsed time: 3240.9796 msecs"
+  (time (def results (scan "https://google.com" {:limit 100 :throttle-amt [100 50000]}))) ;"Elapsed time: 3240.9796 msecs"
   ;;"Elapsed time: 2001.3641 msecs"
   ;;"Elapsed time: 12601.2499 msecs"
   ;;"Elapsed time: 3240.9796 msecs"
@@ -98,7 +111,7 @@
   (defn example-filter [s]
     (str/ends-with? s "/example"))
 
-  (def results (scan "https://example.com/" {:inc-filters [example-filter] :timeout 1000}))
+  (def results (scan "https://example.com/" {:inc-filters [example-filter] :timeout 1000 :throttle 1000}))
   (map first results)
   )
 
